19e61388c94b89a291a15c08a101a1d509196fdd
NoscanX <j.michalski7@gmail.com>
2023-01-22 13:39:29 +0100

    Front tests

--- Frontend/src/tests/UnitTests.test.js
+++ Frontend/src/tests/UnitTests.test.js
@@ -2,19 +2,13 @@ import { render, screen, configure } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 import Articles from "../pages/Articles";
+import Footer from "../components/Footer";
+import Categories from "../components/Categories";
 
 const MockedArticlePage = () => {
   return (
     <BrowserRouter>
       <Articles />
-    </BrowserRouter>
-  );
-};
-
-const MockedHomePage = () => {
-  return (
-    <BrowserRouter>
-      <AdminPage />
     </BrowserRouter>
   );
 };
@@ -25,4 +19,24 @@ it("contains propper header in articles", () => {
     `Regulamin internetowego serwisu ogłoszeniowego OCW`
   );
   expect(headerElement).toBeInTheDocument();
+});
+
+it("should render footer text", () => {
+  render(<Footer />);
+  const headingElement = screen.getByText("Strona wykonana przez:");
+  const headingNameElement = screen.getByText("Jacek Michalski");
+  expect(headingElement).toBeInTheDocument();
+  expect(headingNameElement).toBeInTheDocument();
+});
+
+it("should render footer logo", () => {
+  render(<Footer />);
+  const headingElement = screen.getByText("OCW");
+  expect(headingElement).toBeInTheDocument();
+});
+
+it("should render main website name text", () => {
+  render(<Categories />);
+  const headingElement = screen.getByText("Internetowy serwis ogłoszeniowy");
+  expect(headingElement).toBeInTheDocument();
 });
